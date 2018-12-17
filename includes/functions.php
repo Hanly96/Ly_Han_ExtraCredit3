701fc13cbd9e82dd4a4b0c53c158e9a21f48680b
Your Name <you@example.com>
2018-12-17 14:52:36 -0500

    functions.php

---
+++ includes/functions.php
@@ -0,0 +1,45 @@
+<?php
+include 'connect.php';
+
+function get_single_video($pdo, $vid) {
+    $query = "SELECT * FROM video WHERE id = '$vid'";
+
+    $get_video = $pdo->query($query);
+    $results = array();
+
+    while($row = $get_video->fetch(PDO::FETCH_ASSOC)) {
+        $results[] = $row;
+
+    }
+
+    return $results;
+}
+
+function get_all_videos($pdo) {
+    $query = "SELECT * FROM video";
+
+    $get_video = $pdo->query($query);
+    $results = array();
+
+    while($row = $get_video->fetch(PDO::FETCH_ASSOC)) {
+        $results[] = $row;
+    }
+
+    return $results;
+}
+
+function get_more_details($pdo, $details) {
+    $query = "SELECT * FROM details WHERE details_id = '$details'";
+
+    $get_details = $pdo->query($query);
+    $results = array();
+
+    while($row = $get_details->fetch(PDO::FETCH_ASSOC)) {
+        $results[] = $row;
+
+    }
+
+    return $results;
+}
+
+?>
