f2413b68c37f7fbd2ae8cddb1cf546dc5734ecac
Your Name <you@example.com>
2018-12-17 14:50:07 -0500

    connect.php and index.php created

---
+++ includes/index.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'functions.php';
+
+if (isset($_GET['movie'])) {
+    // pass the connection and the movie id to a function
+    $data = get_single_video($conn, $_GET['movie']);
+    echo json_encode($data);
+}
+
+elseif (isset($_GET['details'])) {
+    $data = get_more_details($conn, $_GET['details']);
+    echo json_encode($data);
+}
+
+else {
+    // pass the connection and the movie id to a function
+    $data = get_all_videos($conn);
+    echo json_encode($data);
+}
+
+?>
